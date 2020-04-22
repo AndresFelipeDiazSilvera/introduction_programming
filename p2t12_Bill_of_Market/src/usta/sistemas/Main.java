708b8e7d99d1cb37f41152f77d573a2e919f7b9d
Andres Felipe Diaz Silvera <andres.diazs@usantoto.edu.co>
2020-04-22 16:52:06 -0500

    p2t12_Bill_of_market
    Bill of market

---
+++ p2t12_Bill_of_Market/src/usta/sistemas/Main.java
@@ -0,0 +1,57 @@
+package usta.sistemas;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/*AUTHOR: Andres Felipe Diaz Silvera
+	  DATE: 2020 - april - 21
+	  DESCRIPTION: This software calculates the total of products with IVA included.
+	 */
+	f_menu();
+	int N = f_total_products();
+	double total_bill=0;
+	for (int i=1;i<=N;i=i+1){
+	    total_bill = total_bill+f_value_product(i);
+    }
+        System.out.println("THE Total bill is: " +total_bill);
+    }
+    public static void f_menu() {
+        //Description: This method show the menu of this software
+        System.out.println("--------------------------");
+        System.out.println("|    SoftBillMarket      |");
+        System.out.println("|Version 1.0 20200421    |");
+        System.out.println("|Created by: Andres Diaz |");
+        System.out.println("--------------------------");
+    }
+    public static int f_total_products(){
+        //Description: This method return the number of total products.
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Input the number of products: ");
+        int total_products = keyboard.nextInt();
+        while (total_products<1){
+            System.err.println("ERR: The total of the products must be greater than one.");
+            System.out.println("Input again the number of products:");
+            total_products = keyboard.nextInt();
+        }
+        return total_products;
+    }
+    public static double f_value_product(int i){
+        //Description: This method return the value total of the product.
+        System.out.println("Input the value of product ("+i+"): ");
+        Scanner keyboard = new Scanner(System.in);
+        double value_product = keyboard.nextDouble();
+        while (value_product<=0){
+            System.err.println("ERR: The value of product must be greater than zero.");
+            System.out.println("Input the value of product ("+i+"): ");
+            value_product = keyboard.nextDouble();
+        }
+        if (value_product>1000){
+             value_product= value_product*1.19;
+        }
+        return value_product;
+    }
+
+
+}
